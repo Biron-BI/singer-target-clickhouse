f7b3164b148ccfd8125fb008d17b85d71a96e78b
Sébastien <sebastien@birondata.com>
2023-09-22 10:31:22 +0200

    add --input parameter

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,23 @@ import {log_error, log_info, parse_args, set_level, set_prefix} from "singer-nod
 import {List} from "immutable"
 import {processStream} from "./processStream"
 import {Config} from "./Config"
+import * as fs from "fs"
 
 const args = parse_args(List(["database", "host", "port", "username", "password"]), List([{
   flags: "-u | --update-streams <tables...>",
   description: "Schema whose root and children tables will be dropped / recreated on SCHEMA messages",
+},{
+  flags: "--input <file>",
+  description: "An alternate file to read from, instead of STDIN",
 }]))
 
 const config = new Config(args.config, List(args.opts.updateStreams as string[] ?? []))
 set_level(config.log_level)
 set_prefix("TARGET")
 
-processStream(process.stdin, config).then(() => {
+const stdin = args.opts.input ? fs.createReadStream(args.opts.input as string) : process.stdin
+
+processStream(stdin, config).then(() => {
   log_info("Stream processing done")
 }).catch((err) => {
   log_error(`${err}`)
