3c9c7c2f1e929b95a1a997f3e36c6ca07bce044f
Sébastien <sebastien@birondata.com>
2025-01-20 08:36:00 +0100

    enhance deploy script

--- deploy_latest.sh
+++ deploy_latest.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+set -o errexit
+set -o pipefail
+
 TAG=$(npm pkg get version | sed 's/"//g')
 
+# requires a previous call to `npm publish`
 docker build --build-arg TAG="$TAG" -t ghcr.io/biron-bi/target-clickhouse:"$TAG" .
 
-#Require login
-#https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-container-registry#authenticating-to-the-container-registry
+# requires login
+# https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-container-registry#authenticating-with-a-personal-access-token-classic
 # minimal scope for PAT is "write:packages"
 docker push ghcr.io/biron-bi/target-clickhouse:"$TAG"
 
